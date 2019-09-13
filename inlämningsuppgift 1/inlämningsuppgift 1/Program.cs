c232fa86ac0e4e41fd70224c1a5673d0b361eb7a
Kemal Cikota <susa02001@utb.vaxjo.se>
2019-09-13 08:54:09 +0200

    Skrivit klart mitt första försök på koden

--- inlämningsuppgift 1/inlämningsuppgift 1/Program.cs
+++ inlämningsuppgift 1/inlämningsuppgift 1/Program.cs
@@ -6,7 +6,33 @@ namespace inlämningsuppgift_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random Random = new Random();
+            int num = Random.Next(1, 101);
+            int f = 0;
+            bool loop=true;
+            Console.WriteLine("Gissa ett tal mellan 1 och 100");
+            while (loop == true)  {
+                int g=convert.ToInt32(Console.ReadLine());
+                f++;
+                if (g == num)
+                {
+                    Console.WriteLine ("Rätt :)");
+                    Console.WriteLine ("Du gjorde  det på: "+f+" försök");
+                    loop = false;
+                }
+                else if (g <= num)
+                {
+                    Console.WriteLine ("För lågt");
+                }
+                else if (g >= num)
+                {
+                    console.WriteLine("För högt");
+                }
+                else {
+                    Console.WriteLine ("Inte ens ett nummer");
+                }
+                }
+            }
         }
     }
 }
